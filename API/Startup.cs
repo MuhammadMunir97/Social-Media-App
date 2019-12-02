eb00bb6f430224fdf6a93255b736411d84481cde
Muhammad Munir <MuhammadShaMunir@gmail.com>
2019-12-02 14:53:44 -0600

    added react for client side and added CORS policy for api

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,12 @@ namespace API
                 opt.UseSqlite(Configuration.GetConnectionString("Default"));
             });
             services.AddControllers();
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader()
+                    .AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,12 +47,14 @@ namespace API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
+            app.UseCors("CorsPolicy");
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
